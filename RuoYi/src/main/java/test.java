ab107b013b0ce9b026aa1f266b2360e42392984b
iceloop <870285972@qq.com>
2019-07-30 15:56:10 +0800

    niu

--- RuoYi/src/main/java/test.java
+++ RuoYi/src/main/java/test.java
@@ -3,6 +3,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("edgfvdlfnklc");
         System.out.println("sdsfsdsdjmsldjsldjsljlsjlfdsjldjfsljdjdljljldfjgldfl");
+        System.out.println("weifuwu");
     }
 
 
