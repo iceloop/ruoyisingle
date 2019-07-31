c8918f12720bbbd2542c3e9e64fac5fd6c078b03
iceloop <870285972@qq.com>
2019-07-31 18:29:53 +0800

    niu

--- RuoYi/src/main/java/test.java
+++ RuoYi/src/main/java/test.java
@@ -1,9 +1,9 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("edgfvdlfnklc");
-        System.out.println("sdsfsdsdjmsldjsldjsljlsjlfdsjldjfsljdjdljljldfjgldfl");
-        System.out.println("weifuwu");
+        String password = "AA0000fff111";
+        String regex = "^(?:(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])).{6,12}$";
+        System.out.println(password.matches(regex));
     }
 
 
